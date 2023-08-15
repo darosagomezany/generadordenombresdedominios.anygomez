c8c6f9b80cfa3699a7ae03cd0b3985f7cc08c54a
darosagomezany <139292566+darosagomezany@users.noreply.github.com>
2023-08-15 17:32:21 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['yo', 'tu', 'el', 'nosotros', 'vosotros', 'ellos'];
+let noun = ['mago', 'mapache', 'arbol', 'invocador', 'sniper', 'camper', 'silla', 'taza'];
+let adj = ['super', 'alto', 'hondo', 'cansado', 'bello', 'redondo', 'magico', 'astral'];
+let ext = ['.cl', '.com', '.com.ar', '.com.nz'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function domainNameGenerator (pronoun, noun, adj, ext){
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    let numPronoun = Math.floor(Math.random() * pronoun.length);
+    let numNoun = Math.floor(Math.random() * noun.length);
+    let numAdj = Math.floor(Math.random() * adj.length);
+    let numExt = Math.floor(Math.random() * ext.length);
+
+    return pronoun[numPronoun] + noun[numNoun] + adj[numAdj] + ext[numExt];
+}
+
+function onLoad(){
+    let domainGen = document.getElementById('domainGenerator');
+    domainGen.innerHTML = domainNameGenerator(pronoun, noun, adj, ext);
+}
+
+window.onload = onLoad;
